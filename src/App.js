a5ebd0e357875d4635d64d4dcae10a4c9c2349d8
s0npaRi11 <paragmahale36@gmail.com>
2021-07-14 14:16:18 +0530

    react.lazy added for routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 
@@ -7,17 +7,29 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 // import '../public/actions'
 
-import Home from './pages/home'
-import PrivacyPolicy from './pages/privacyPolicy'
-import ReturnPolicy from './pages/returnPolicy'
-import TermsAndConditions from './pages/termsAndConditions'
-import Services from './pages/services'
-import Contact from './pages/contact'
-import FAQ from './pages/faq'
-import About from './pages/about'
-import Complaint from './pages/complaint'
-import WebDev from './pages/websiteDevService'
-import Ecom from './pages/eCommerceService'
+// import Home from './pages/home'
+// import PrivacyPolicy from './pages/privacyPolicy'
+// import ReturnPolicy from './pages/returnPolicy'
+// import TermsAndConditions from './pages/termsAndConditions'
+// import Services from './pages/services'
+// import Contact from './pages/contact'
+// import FAQ from './pages/faq'
+// import About from './pages/about'
+// import Complaint from './pages/complaint'
+// import WebDev from './pages/websiteDevService'
+// import Ecom from './pages/eCommerceService'
+
+const Home = lazy(() => import('./pages/home'));
+const PrivacyPolicy = lazy(() => import('./pages/privacyPolicy'))
+const ReturnPolicy = lazy(() => import('./pages/returnPolicy'))
+const TermsAndConditions = lazy(() => import('./pages/termsAndConditions'))
+const Services = lazy(() => import('./pages/services'))
+const Contact = lazy(() => import('./pages/contact'))
+const FAQ = lazy(() => import('./pages/faq'))
+const About = lazy(() => import('./pages/about'))
+const Complaint = lazy(() => import('./pages/complaint'))
+const WebDev = lazy(() => import('./pages/websiteDevService'))
+const Ecom = lazy(() => import('./pages/eCommerceService'))
 
 function App() {
   return (
@@ -29,53 +41,55 @@ function App() {
       </Helmet>
 
       <Router basename="vipash-tech-website">
-        <Switch>
+        <Suspense fallback={<div className="loader"> Loading.... </div>}>
+          <Switch>
 
-          <Route exact path="/">
-            <Home /> 
-          </Route>
+            <Route exact path="/">
+              <Home /> 
+            </Route>
 
-          <Route path="/contact">
-            <Contact />
-          </Route>
+            <Route path="/contact">
+              <Contact />
+            </Route>
 
-          <Route exact path="/services">
-            <Services />
-          </Route>
+            <Route exact path="/services">
+              <Services />
+            </Route>
 
-          <Route exact path="/services/web-development">
-            <WebDev />
-          </Route>
+            <Route exact path="/services/web-development">
+              <WebDev />
+            </Route>
 
-          <Route exact path="/services/ecommerce-development">
-            <Ecom />
-          </Route>
+            <Route exact path="/services/ecommerce-development">
+              <Ecom />
+            </Route>
 
-          <Route path="/about">
-            <About />
-          </Route>
+            <Route path="/about">
+              <About />
+            </Route>
 
-          <Route path="/privacy-policy">
-            <PrivacyPolicy />
-          </Route>
+            <Route path="/privacy-policy">
+              <PrivacyPolicy />
+            </Route>
 
-          <Route path="/return-policy">
-            <ReturnPolicy />
-          </Route>
+            <Route path="/return-policy">
+              <ReturnPolicy />
+            </Route>
 
-          <Route path="/terms-and-conditions">
-            <TermsAndConditions />
-          </Route>
+            <Route path="/terms-and-conditions">
+              <TermsAndConditions />
+            </Route>
 
-          <Route path="/faq">
-            <FAQ />
-          </Route>
+            <Route path="/faq">
+              <FAQ />
+            </Route>
 
-          <Route path="/complaint">
-            <Complaint />
-          </Route>
+            <Route path="/complaint">
+              <Complaint />
+            </Route>
 
-        </Switch>
+          </Switch>
+        </Suspense>
       </Router>
 
     </React.Fragment>
